1b98b19063386078c89b5ec0da8682ad97ba4ee5
dfcold <3135287831@qq.com>
2024-12-04 03:35:18 +0800

    feat: 初步添加jwt

---
+++ src/main/java/com/dfcold/whulibbackend/common/ResultCode.java
@@ -0,0 +1,13 @@
+package com.dfcold.whulibbackend.common;
+
+public interface ResultCode {
+
+    /**
+     * 成功
+     */
+    public static Integer SUCCESS = 20000;
+    /**
+     * 失败
+     */
+    public static Integer ERROR = 20001;
+}
--- src/main/java/com/dfcold/whulibbackend/controller/LibraryProxyController.java
+++ src/main/java/com/dfcold/whulibbackend/controller/LibraryProxyController.java
@@ -1,5 +1,11 @@
 package com.dfcold.whulibbackend.controller;
 
+import com.dfcold.whulibbackend.common.Result;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -7,7 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
  * 代理访问图书馆资源
  */
 @RestController
+@RequestMapping("/proxy")
 public class LibraryProxyController {
+    @Operation(summary = "图书馆登录", description = "登录图书馆")
+    @RequestMapping("/login")
+    public Result login(@AuthenticationPrincipal Authentication authentication){
+        return Result.ok();
 
-
+    }
 }
